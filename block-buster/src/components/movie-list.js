e437034d20955d9da5687068400a9ec1cf7ec593
Juan Fourment <fourmentjuan@gmail.com>
2021-03-16 14:13:04 -0300

    se agrega redux

--- block-buster/src/components/movie-list.js
+++ block-buster/src/components/movie-list.js
@@ -2,17 +2,9 @@ import { Component, createElement} from '../lib/react/index.js'
 import styled from '../lib/styled-component.js'
 import Wrapper from './wrapper.js'
 import Movie from './movie.js'
-import movies from '../movies.js'
-/*<div class="wrapper">
-    <section class="movie-list" id="container">
-      <article class="movie recommended">
-        <img class="movie-poster" src="//image.tmdb.org/t/p/w220_and_h330_face/ugZW8ocsrfgI95pnQ7wrmKDxIe.jpg" alt="">
-        <p class="movie-title">Duro de matar</p>
-        <p class="movie-id">123456</p>
-        <span class="movie-rate">10</span>
-      </article>
-    </section>
-  </div> --></div>*/
+import store from '../store.js'
+//import movies from '../movies.js'
+
 
 const MovieListStyled = styled.section`
   display: grid;
@@ -23,7 +15,11 @@ const MovieListStyled = styled.section`
 `
 
 class MovieList extends Component{
+  state = {
+    movies:store.getState().movieList
+  }
   render(){
+    const { movies } = this.state;
     return  Wrapper({
       children: MovieListStyled({
         children:movies.map(movie => new Movie(movie))
---
+++ block-buster/src/redux/index.js
@@ -0,0 +1,27 @@
+const createStore = (reducer, initialState) => {
+  let state = initialState
+  let updater = () => {}
+  const getState = () => state
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    updater()
+  };
+  const subscribe = (listener) => {
+    updater = listener
+  };
+  return {
+    getState,
+    dispatch,
+    subscribe,
+  }
+}
+
+function combineReducers() {
+  
+}
+
+export{
+  createStore,
+  combineReducers
+}
